c9c062ca09496521343b1bc584717088b183fadc
Ubuntu <ubuntu@ip-172-31-90-130.ec2.internal>
2019-07-14 14:04:04 +0000

    added search autocomplete

--- scripts/script.js
+++ scripts/script.js
@@ -1,62 +1,123 @@
 // alert("it works!")
 
 
+// Map Initialisation
 function initMap() {
+
+  // Initial LatLng
+  let myLatLng = { lat: 1.290270, lng: 103.851959 }
+
+
   // The map, centered at Singapore
-  var map = new google.maps.Map(
-    document.getElementById('map'), { zoom: 4, center: { lat: 1.290270, lng: 103.851959 } });
+  let map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 10,
+    center: myLatLng
+  });
+
+
   // The marker, positioned at Singapore
-  var marker = new google.maps.Marker({ position: { lat: 5.290270, lng: 102.851959 }, map: map });
-}
+  let marker = new google.maps.Marker({
+    position: myLatLng,
+    map: map,
+    title: 'Begin planning your trips!'
+  });
 
 
-function findMe(){
-  map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: -34.397, lng: 150.644},
-          zoom: 4
-        });
-        infoWindow = new google.maps.InfoWindow;
+  // Autocomplete box
+  var autocompleteSearch = document.getElementById('search-location')
+  var autocomplete = new google.maps.places.Autocomplete(autocompleteSearch);
 
-        // Try HTML5 geolocation.
-        if (navigator.geolocation) {
-          navigator.geolocation.getCurrentPosition(function(position) {
-            var pos = {
-              lat: position.coords.latitude,
-              lng: position.coords.longitude
-            };
-
-            infoWindow.setPosition(pos);
-            infoWindow.setContent('Location found.');
-            infoWindow.open(map);
-            map.setCenter(pos);
-          }, function() {
-            handleLocationError(true, infoWindow, map.getCenter());
-          });
-        } else {
-          // Browser doesn't support Geolocation
-          handleLocationError(false, infoWindow, map.getCenter());
-        }
-      }
-
-      function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-        infoWindow.setPosition(pos);
-        infoWindow.setContent(browserHasGeolocation ?
-                              'Error: The Geolocation service failed.' :
-                              'Error: Your browser doesn\'t support geolocation.');
-        infoWindow.open(map);
-      
 }
 
 
 
-// function searchLocation(){
-//   alert("location search!");
+// Find me function
+function findMe() {
+
+  let map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 10,
+  });
+
+  // Infowindow for find me function
+  let infoWindow = new google.maps.InfoWindow;
+  // Geolocation.
+
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var pos = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
+      // console.log(pos)
+      infoWindow.setPosition(pos);
+      infoWindow.setContent('Location found.');
+      infoWindow.open(map);
+      map.setCenter(pos);
+    }, function() {
+      handleLocationError(true, infoWindow, map.getCenter());
+    });
+  }
+  else {
+    // Browser doesn't support Geolocation
+    handleLocationError(false, infoWindow, map.getCenter());
+  }
+}
+
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+  infoWindow.setPosition(pos);
+  infoWindow.setContent(browserHasGeolocation ?
+    'Error: The Geolocation service failed.' :
+    'Error: Your browser doesn\'t support geolocation.');
+  infoWindow.open(map);
+
+}
+
+
+
+
+
+// searchLocation function
+function searchLocation(location) {
+
+  let searchLocationRequest = document.getElementById("search-location").value
+
+
+
+  mapSearch = new google.maps.places.PlacesService(map);
+
+
+
+}
+//   mapSearch.findPlaceFromQuery(searchLocationRequest, function(results, status) {
+//     if (status === google.maps.places.PlacesServiceStatus.OK) {
+//       for (var i = 0; i < results.length; i++) {
+//         createMarker(results[i]);
+//       }
+
+//       map.setCenter(results[0].geometry.location);
+//     }
+//   });
 // }
 
-axios.get("https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=500&types=food&name=harbour&key=AIzaSyA_Vfu-FYPJTrpiFgnNOaUP5jrtbeyJflk")
-  .then(function (response) {
+// function createMarker(place) {
+//   var marker = new google.maps.Marker({
+//     map: map,
+//     position: place.geometry.location
+//   });
+
+//   google.maps.event.addListener(marker, 'click', function() {
+//     infowindow.setContent(place.name);
+//     infowindow.open(map, this);
+//   });
+// }
+
+
+
+
+
+axios.get("https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=500&types=food&name=harbour&key=AIzaSyDoEv5jmhi5Iw1bPJTBGAEAWUsS-BQnBro")
+  .then(function(response) {
     // handle success
     console.log(response);
   })
-  
-  
